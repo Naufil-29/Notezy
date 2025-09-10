0e2378934e72e442a48b6592c71eb3397a3f9c9d
Naufil-29 <shaikhnaufil2908@gmail.com>
2025-09-10 16:55:38 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot, } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { ThemeProvider } from './components/theme-Provider.jsx';
+import { ThemeProvider } from './components/Theme-Provider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
