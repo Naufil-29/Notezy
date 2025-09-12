40e4c2952dc2b7f52ed7d90d7882844df238c672
Naufil-29 <shaikhnaufil2908@gmail.com>
2025-09-12 11:25:43 +0530

    Update ProfileBar.jsx

--- Frontend/src/components/ProfileBar.jsx
+++ Frontend/src/components/ProfileBar.jsx
@@ -130,7 +130,7 @@ const ProfileBar = () => {
             previewImg
               ? previewImg
               : user?.profileImg
-                ? `${import.meta.env.VITE_BACKEND_URL}/uploads/${user.profileImg}`
+                ? `https://notezy-backend-lz8r.onrender.com/uploads/${user.profileImg}`
                 : defaultImg
           }
           alt="profile"
