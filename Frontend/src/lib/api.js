1c3ce1595970d4afaf23663c9e44f7192666bd5c
Naufil-29 <shaikhnaufil2908@gmail.com>
2025-09-12 11:41:53 +0530

    Update api.js

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://notezy-backend-lz8r.onrender.com"
+  baseURL: "https://notezy-backend-lz8r.onrender.com/api"
 });
 
 // Add token to every request if exists // Attach token automatically (for future protected calls)
