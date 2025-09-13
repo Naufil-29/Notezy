50c4438450963dae44676c6cb42c377e160fad4d
Naufil-29 <shaikhnaufil2908@gmail.com>
2025-09-13 09:56:17 +0530

    Update NoteDetailPage.jsx

--- Frontend/src/Pages/NoteDetailPage.jsx
+++ Frontend/src/Pages/NoteDetailPage.jsx
@@ -67,7 +67,7 @@ const handleDelete = async () => {
         if(!window.confirm("Are you sure you want to delete this note")) return;
 
         try{ 
-            await api.delete(`/notes/${id}`)
+            await api.delete(`/api/notes/${id}`)
             toast.success("Note deleted sucessfully!")
             navigate("/notes")
         }catch(error){ 
@@ -84,7 +84,7 @@ const handleDelete = async () => {
     setSaving(true)
 
     try{ 
-        await api.put(`notes/${id}`, note)
+        await api.put(`/api/notes/${id}`, note)
         toast.success("Note updated successfully")
         navigate("/notes")
     }catch(error){ 
